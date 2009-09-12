fb536b7ca6e51c39698c1e05e40c4958d8c2091d
Jakob Skjerning <jakob@mentalized.net>
2009-09-12 20:38:29 +0200

    The player can now move

--- game.rb
+++ game.rb
@@ -35,11 +35,31 @@ class Player
     @x = window.width / 2
     @y = window.height / 2
     @angle = 0
+    
+    @speed_x = 0
+    @speed_y = 0
 
     @image = Gosu::Image.new(window, 'resources/graphics/player.png')
   end
   
   def update
+    # Rotate left and right
+    @angle += 3 if window.button_down? Gosu::Button::KbRight
+    @angle -= 3 if window.button_down? Gosu::Button::KbLeft
+
+    # Increase the speed along the axis
+    if window.button_down? Gosu::Button::KbUp
+      @speed_x += Gosu::offset_x(self.angle, 0.5)
+      @speed_y += Gosu::offset_y(self.angle, 0.5)
+    end
+
+    # Friction
+    @speed_x = @speed_x * 0.97
+    @speed_y = @speed_y * 0.97
+
+    # Move the player
+    @x += @speed_x
+    @y += @speed_y
   end
   
   def draw
